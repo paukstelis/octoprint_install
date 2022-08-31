b93bf14970d4c64672b1bc3c876d179724b12476
paukstelis <paukstel@umd.edu>
2022-08-31 10:14:22 -0400

    first commit

---
+++ plugins.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+install_plugin() {
+    echo "Installing $plugin...." | log
+    $OCTOPIP install "$plugin_path"
+}
+
+plugin_menu() {
+    echo
+    echo
+    PS3='Select recommended plugins to install: '
+    readarray -t plugins < <(cat $SCRIPTDIR/plugins_list | sed -n -e 's/^plugin:\(.*\) path:.*/\1/p')
+    plugins+=("All")
+    plugins+=("Quit")
+    select plugin in "${plugins[@]}"
+    do
+        if [ "$plugin" == Quit ]; then
+            break
+            
+        fi
+        
+        #some special thing to do if All Recommended
+        if [ "$plugin" == All ]; then
+            for plugin in "${plugins[@]}"; do
+                plugin_path=$(cat $SCRIPTDIR/plugins_list | sed -n -e "s/^plugin:$plugin path:\([[:graph:]]*\)/\1/p")
+                if [ -n "$plugin_path" ]; then
+                    install_plugin $plugin $plugin_path
+                fi
+            done
+            break
+        fi
+        #install single plugin
+        #get plugin path
+        plugin_path=$(cat $SCRIPTDIR/plugins_list | sed -n -e "s/^plugin:$plugin path:\([[:graph:]]*\)/\1/p")
+        install_plugin 
+    done
+    
+}
+
